752320db292eba5a48ef06bc9fb3bc4250e15861
Pablo Guelfi <pguelfi@gmail.com>
2024-09-27 12:11:15 +0200

    All routes and controllers

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,34 @@
+class BookmarksController < ApplicationController
+
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+    @bookmark.list = @list
+    @movies = Movie.all
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list), notice: 'Bookmark was successfully created.'
+    else
+      @bookmark = Bookmark.new
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @list = @bookmark.list
+    @bookmark.destroy
+    redirect_to list_path(@list), notice: 'Bookmark was successfully removed.'
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'bookmarks/new'
+  get 'bookmarks/create'
+  get 'bookmarks/destroy'
+  get 'lists/index'
+  get 'lists/show'
+  get 'lists/new'
+  get 'lists/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -7,4 +14,24 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  root to: "lists#index"
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
+
+# Root path
+# get "/", to: "lists#index", as: :root
+
+# Lists routes
+# get "/lists", to: "lists#index", as: :lists         # Lists index (view all lists)
+# get "/lists/new", to: "lists#new", as: :new_list    # Form for creating a new list
+# post "/lists", to: "lists#create"                   # Action to create a new list
+# get "/lists/:id", to: "lists#show", as: :list
+
+# Nested routes for bookmarks
+# get "/lists/:list_id/bookmarks/new", to: "bookmarks#new", as: :new_list_bookmark
+# post "/lists/:list_id/bookmarks", to: "bookmarks#create"
+
+# delete "/bookmarks/:id", to: "bookmarks#destroy", as: :bookmark
